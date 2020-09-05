68777fc56c6c27fa392c37fd29e11bf1685f8a7c
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2020-09-05 19:24:14 +0200

    Improve testcase

--- frameworks/spring-boot-starter-logbook/src/test/java/com/github/skjolber/jsonfilter/spring/logbook/PathRequestFilterTest.java
+++ frameworks/spring-boot-starter-logbook/src/test/java/com/github/skjolber/jsonfilter/spring/logbook/PathRequestFilterTest.java
@@ -1,22 +1,31 @@
 package com.github.skjolber.jsonfilter.spring.logbook;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.zalando.logbook.HttpRequest;
 import org.junit.jupiter.api.Test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.skjolber.jsonfilter.JsonFilter;
 import com.github.skjolber.jsonfilter.core.DefaultJsonLogFilterBuilder;
 import com.github.skjolber.jsonfilter.spring.RequestResponseJsonFilter;
 import com.github.skjolber.jsonfilter.spring.matcher.JsonFilterPathMatcher;
 import com.github.skjolber.jsonfilter.spring.matcher.PrefixJsonFilterPathMatcher;
 
+import static com.google.common.truth.Truth.*;
+
 public class PathRequestFilterTest {
 
 	@Test
-	public void testFilter() {
+	public void testFilter() throws Exception {
 		
 		List<JsonFilterPathMatcher> requests = new ArrayList<>();
 		List<JsonFilterPathMatcher> responses = new ArrayList<>();
@@ -38,6 +47,36 @@ public class PathRequestFilterTest {
 		assertNotNull(requestResponseJsonFilter.getResponseFilter("/def"));
 		assertNull(requestResponseJsonFilter.getRequestFilter("/def"));
 		
+		PathRequestFilter filter = new PathRequestFilter(requestResponseJsonFilter);
+
+		HttpRequest miss = mock(HttpRequest.class);
+		when(miss.getContentType()).thenReturn("application/json");
+		when(miss.getPath()).thenReturn("/xyz");
+		assertThat(filter.filter(miss)).isNotInstanceOf(JsonFilterHttpRequest.class);
 		
+		HttpRequest other = mock(HttpRequest.class);
+		when(other.getContentType()).thenReturn("application/xml");
+		assertThat(filter.filter(other)).isNotInstanceOf(JsonFilterHttpRequest.class);
+
+		HttpRequest match = mock(HttpRequest.class);
+		when(match.getContentType()).thenReturn("application/json");
+		when(match.getPath()).thenReturn("/abc");
+
+		HttpRequest filtering = filter.filter(match);
+		assertThat(filtering).isInstanceOf(JsonFilterHttpRequest.class);
+
+		Map<String, Object> document = new HashMap<>();
+		document.put("firstName", "secret1");
+		document.put("lastName", "secret2");
+		ObjectMapper mapper = new ObjectMapper();
+		
+		when(match.getBodyAsString()).thenReturn(mapper.writeValueAsString(document));
+		when(match.getBody()).thenReturn(mapper.writeValueAsBytes(document));
+		
+		Map readValue1 = mapper.readValue(filtering.getBody(), Map.class);
+		assertThat(readValue1.get("firstName")).isEqualTo("*****");
+		
+		Map readValue2 = mapper.readValue(filtering.getBodyAsString(), Map.class);
+		assertThat(readValue2.get("firstName")).isEqualTo("*****");
 	}
 }
