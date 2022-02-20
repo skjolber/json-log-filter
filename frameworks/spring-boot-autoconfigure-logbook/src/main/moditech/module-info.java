83efdd682cd6b62b80362725d4509ec7139c44bf
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2022-02-20 21:07:06 +0100

    Fix release

--- frameworks/spring-boot-autoconfigure-logbook/src/main/moditech/module-info.java
+++ frameworks/spring-boot-autoconfigure-logbook/src/main/moditech/module-info.java
@@ -1,5 +1,8 @@
-module com.github.skjolber.jsonfilter.logbook {
+module com.github.skjolber.jsonfilter.spring.autoconfigure.logbook {
 	requires com.github.skjolber.jsonfilter.path;
+	requires com.github.skjolber.jsonfilter;
+	requires com.github.skjolber.jsonfilter.jackson;
+	requires com.github.skjolber.jsonfilter.core;
 	
-	exports com.github.skjolber.jsonfilter.spring.logbook;
+	exports com.github.skjolber.jsonfilter.spring.autoconfigure.logbook;
 }
--- frameworks/spring-boot-starter-logbook/src/main/moditech/module-info.java
+++ frameworks/spring-boot-starter-logbook/src/main/moditech/module-info.java
@@ -1,5 +1,9 @@
 module com.github.skjolber.jsonfilter.logbook {
+	requires com.github.skjolber.jsonfilter;
 	requires com.github.skjolber.jsonfilter.path;
+	requires com.github.skjolber.jsonfilter.jackson;
+	requires com.github.skjolber.jsonfilter.core;
+	requires com.github.skjolber.jsonfilter.spring.autoconfigure.logbook;
 	
 	exports com.github.skjolber.jsonfilter.spring.logbook;
 }
