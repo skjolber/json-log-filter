48cc65edacb9e46b3c8faa246dbb769ad9979801
Thomas Skjølberg <thomas.skjolberg@gmail.com>
2022-05-29 01:32:32 +0200

    Add more testing

---
+++ support/test/src/main/java/com/github/skjolber/jsonfilter/test/JsonPathFilter.java
@@ -0,0 +1,265 @@
+package com.github.skjolber.jsonfilter.test;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ContainerNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.databind.node.TextNode;
+import com.github.skjolber.jsonfilter.JsonFilter;
+import com.jayway.jsonpath.Configuration;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.Option;
+import com.jayway.jsonpath.ParseContext;
+import com.fasterxml.jackson.databind.node.ValueNode;
+import com.jayway.jsonpath.spi.json.JacksonJsonNodeJsonProvider;
+import com.jayway.jsonpath.spi.mapper.JacksonMappingProvider;
+
+public class JsonPathFilter implements JsonFilter {
+
+	private static JsonPath ANY = JsonPath.compile("$..*");
+	
+	public static final String FILTER_PRUNE_MESSAGE = "SUBTREE REMOVED";
+	public static final String FILTER_ANONYMIZE = "*****";
+	public static final String FILTER_TRUNCATE_MESSAGE = "...TRUNCATED BY ";
+	
+    private static final ParseContext CONTEXT = JsonPath.using(
+            Configuration.builder()
+                    .jsonProvider(new JacksonJsonNodeJsonProvider())
+                    .mappingProvider(new JacksonMappingProvider())
+                    .options(Option.SUPPRESS_EXCEPTIONS)
+                    .options(Option.ALWAYS_RETURN_LIST)
+                    .build());
+    
+    public static Builder newBuilder() {
+    	return new Builder();
+    }
+    
+    public static class Builder {
+    	protected List<String> anonymizeFilters = new ArrayList<>();
+    	protected List<String> pruneFilters = new ArrayList<>();
+    	
+    	/** Raw JSON */
+    	protected String pruneJsonValue;
+    	/** Raw JSON */
+    	protected String anonymizeJsonValue;
+    	
+    	/** Raw (escaped) JSON string */
+    	protected String truncateStringValue;
+
+    	protected int maxStringLength = -1;
+
+    	public Builder withMaxStringLength(int length) {
+    		this.maxStringLength = length;
+    		return this;
+    	}	
+
+    	public Builder withPrune(String ... filters) {
+    		Collections.addAll(pruneFilters, filters);
+    		return this;
+    	}
+    	
+    	public Builder withAnonymize(String ... filters) {
+    		Collections.addAll(anonymizeFilters, filters);
+    		return this;
+    	}
+
+    	public JsonPathFilter build() {
+    		return new JsonPathFilter(maxStringLength, anonymizeFilters.toArray(new String[anonymizeFilters.size()]), pruneFilters.toArray(new String[pruneFilters.size()]));
+    	}
+    }
+
+    private final String[] anonPaths;
+    private final String[] prunePaths;
+
+    private final List<JsonPath> anon;
+    private final List<JsonPath> prune;
+    private final int maxStringLength;
+
+    public JsonPathFilter(int maxStringLength, String[] anonymize, String[] prune) {
+    	List<String> anonymizeFilters = anonymize != null ? Arrays.asList(anonymize) : Collections.emptyList();
+    	List<String> pruneFilters = prune != null ? Arrays.asList(prune) : Collections.emptyList();
+    	
+		this.anon = anonymizeFilters.stream().map(a -> JsonPath.compile(convert(a))).collect(Collectors.toList());
+		this.prune = pruneFilters.stream().map(a -> JsonPath.compile(convert(a))).collect(Collectors.toList());
+		
+		this.anonPaths = anonymizeFilters.toArray(new String[anonymizeFilters.size()]);
+		this.prunePaths = pruneFilters.toArray(new String[pruneFilters.size()]);
+		this.maxStringLength = maxStringLength; 
+	}
+
+	private String convert(String str) {
+		String original = str;
+		if(!str.startsWith("$")) {
+			str = '$' + str;
+		}
+		String result = str.replace('/', '.');
+		
+		return result;
+	}
+
+	@Override
+	public String process(char[] chars) {
+		return process(new String(chars));
+	}
+
+	private String filter(DocumentContext original) {
+		original.limit(Integer.MAX_VALUE);
+		
+		for(JsonPath jsonPath : anon) {
+			original = original.map(jsonPath, (currentValue, configuration) -> {
+				if(currentValue instanceof ContainerNode) {
+					ContainerNode containerNode = (ContainerNode)currentValue;
+					anonymize(containerNode);
+					
+					return containerNode;
+				}
+				return FILTER_ANONYMIZE;
+			});
+		}
+		for(JsonPath jsonPath : prune) {
+			original = original.map(jsonPath, (currentValue, configuration) -> {
+				return FILTER_PRUNE_MESSAGE;
+			});
+		}
+		
+		if(maxStringLength != -1) {
+			original = original.map(ANY, (currentValue, configuration) -> {
+				String value;
+				if(currentValue instanceof String) {
+					value = (String)currentValue;
+				} else if(currentValue instanceof TextNode) {
+					TextNode textNode = (TextNode)currentValue;
+					value = textNode.asText();
+				} else {
+					return currentValue;
+				}
+
+				if(value.length() > maxStringLength) {
+					// A high surrogate precedes a low surrogate. Together they make up a codepoint.
+					int maxStringLength = this.maxStringLength;
+					if(Character.isLowSurrogate(value.charAt(maxStringLength))) {
+						maxStringLength--;
+					}
+					int lost = value.length() - maxStringLength;
+					return value.subSequence(0, maxStringLength) + FILTER_TRUNCATE_MESSAGE + lost;
+
+				}
+				return value;
+			});
+		}
+		
+		return original.jsonString();
+	}
+	
+	private void anonymize(ContainerNode containerNode) {
+		if(containerNode instanceof ArrayNode) {
+			ArrayNode arrayNode = (ArrayNode)containerNode;
+			
+			for(int i = 0; i < arrayNode.size(); i++) {
+				JsonNode jsonNode = arrayNode.get(i);
+				
+				if(jsonNode instanceof ContainerNode) {
+					anonymize((ContainerNode)jsonNode);
+				} else if(jsonNode instanceof ValueNode) {
+					ValueNode valueNode = (ValueNode)jsonNode;
+					arrayNode.set(i, TextNode.valueOf(FILTER_ANONYMIZE));
+				}
+			}
+		} else if(containerNode instanceof ObjectNode) {
+			ObjectNode objectNode = (ObjectNode)containerNode;
+			
+			Iterator<String> fieldNames = objectNode.fieldNames();
+			while(fieldNames.hasNext()) {
+				String next = fieldNames.next();
+
+				JsonNode jsonNode = objectNode.findValue(next);
+				if(jsonNode instanceof ContainerNode) {
+					anonymize((ContainerNode)jsonNode);
+				} else {
+					objectNode.put(next, FILTER_ANONYMIZE);
+				}
+			}
+		}
+	}
+
+	@Override
+	public String process(String chars) {
+		try {
+			DocumentContext parse = CONTEXT.parse(chars);
+			return filter(parse);
+		} catch(Exception e) {
+			return null;
+		}
+	}
+
+	@Override
+	public boolean process(String chars, StringBuilder output) {
+		try {
+			DocumentContext parse = CONTEXT.parse(chars);
+			String filter = filter(parse);
+			output.append(filter);
+			return true;
+		} catch(Exception e) {
+			return false;
+		}
+	}
+
+	@Override
+	public boolean process(char[] chars, int offset, int length, StringBuilder output) {
+		if(chars.length < offset + length) {
+			return false;
+		}
+		return process(new String(chars, offset, length), output);
+	}
+
+	@Override
+	public byte[] process(byte[] chars) {
+		return process(chars, 0, chars.length);
+	}
+
+	@Override
+	public byte[] process(byte[] chars, int offset, int length) {
+		String process = process(new String(chars, StandardCharsets.UTF_8));
+		if(process != null) {
+			return process.getBytes(StandardCharsets.UTF_8);
+		}
+		return null;
+	}
+
+	@Override
+	public boolean process(byte[] chars, int offset, int length, ByteArrayOutputStream output) {
+		if(chars.length < offset + length) {
+			return false;
+		}
+		byte[] process = process(chars, offset, length);
+		if(process != null) {
+			output.write(process, offset, length);
+			return true;
+		}
+		return false;
+	}
+
+	public int getMaxStringLength() {
+		return maxStringLength;
+	}
+	
+	
+	public String[] getAnonymizeFilters() {
+		return anonPaths;
+	}
+
+	public String[] getPruneFilters() {
+		return prunePaths;
+	}
+	
+}
---
+++ support/test/src/test/java/com/github/skjolber/jsonfilter/test/JsonPathFilterTest.java
@@ -0,0 +1,123 @@
+package com.github.skjolber.jsonfilter.test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNull;
+
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.function.Predicate;
+
+import org.junit.jupiter.api.Test;
+
+/** 
+ * Verify as much of the test resources as possible.
+ *
+ */
+
+public class JsonPathFilterTest extends DefaultJsonFilterTest {
+
+	protected static final Predicate<String> UNICODE_FILTER =  (json) -> !json.contains("\\");
+	protected static final Predicate<String> ARRAY_FILTER =  (json) -> !json.startsWith("[");
+
+	public JsonPathFilterTest() throws Exception {
+		super(false);
+	}
+
+	@Test
+	public void passthrough_success() throws Exception {
+		assertThat(new JsonPathFilter(-1, null, null)).hasPassthrough();
+		assertThat(new JsonPathFilter(-1, new String[]{PASSTHROUGH_XPATH}, new String[]{PASSTHROUGH_XPATH})).hasPassthrough();
+	}
+
+	@Test
+	public void exception_returns_false() throws Exception {
+		JsonPathFilter filter = new JsonPathFilter(-1, new String[]{PASSTHROUGH_XPATH}, new String[]{PASSTHROUGH_XPATH});
+		assertFalse(filter.process(new char[] {}, 1, 1, new StringBuilder()));
+		assertFalse(filter.process(new byte[] {}, 1, 1, new ByteArrayOutputStream()));
+	}	
+	
+	@Test
+	public void exception_offset_if_not_exceeded() throws Exception {
+		JsonPathFilter filter = new JsonPathFilter(-1, null, null);
+		assertNull(filter.process(TRUNCATED));
+		assertNull(filter.process(TRUNCATED.getBytes(StandardCharsets.UTF_8)));
+		
+		assertFalse(filter.process(FULL, 0, FULL.length - 3, new StringBuilder()));
+		// assertFalse(filter.process(new String(FULL).getBytes(StandardCharsets.UTF_8), 0, FULL.length - 3, new ByteArrayOutputStream()));
+	}
+	
+	@Test
+	public void exception_incorrect_level() throws Exception {
+		JsonPathFilter filter = new JsonPathFilter(127, new String[]{PASSTHROUGH_XPATH}, new String[]{PASSTHROUGH_XPATH});
+		assertFalse(filter.process(INCORRECT_LEVEL, new StringBuilder()));
+		assertNull(filter.process(INCORRECT_LEVEL.getBytes(StandardCharsets.UTF_8)));
+	}
+	
+	@Test
+	public void anonymize() throws Exception {
+		assertThat(new JsonPathFilter(-1, new String[]{DEFAULT_PATH}, null), ARRAY_FILTER).hasAnonymized(DEFAULT_PATH);
+		assertThat(new JsonPathFilter(-1, new String[]{DEFAULT_PATH, PASSTHROUGH_XPATH}, new String[]{PASSTHROUGH_XPATH}), ARRAY_FILTER).hasAnonymized(DEFAULT_PATH);
+		assertThat(new JsonPathFilter(-1, new String[]{DEEP_PATH1, PASSTHROUGH_XPATH}, new String[]{PASSTHROUGH_XPATH}), ARRAY_FILTER).hasAnonymized(DEEP_PATH1);
+	}
+	/*
+	@Test
+	public void anonymizeMaxPathMatches() throws Exception {
+		assertThat(new JsonPathFilter(-1, 1, new String[]{"/key1"}, null)).hasAnonymized("/key1");
+		
+		assertThat(new JsonPathFilter(-1, 1, new String[]{DEFAULT_PATH}, null)).hasAnonymized(DEFAULT_PATH);
+		assertThat(new JsonPathFilter(-1, 2, new String[]{DEFAULT_PATH}, null)).hasAnonymized(DEFAULT_PATH);
+	}
+*/
+	@Test
+	public void anonymizeWildcard() throws Exception {
+		assertThat(new JsonPathFilter(-1, new String[]{DEFAULT_WILDCARD_PATH}, null), ARRAY_FILTER).hasAnonymized(DEFAULT_WILDCARD_PATH);
+	}
+	
+	@Test
+	public void anonymizeAny() throws Exception {
+		assertThat(new JsonPathFilter(-1, new String[]{DEFAULT_ANY_PATH}, null), ARRAY_FILTER).hasAnonymized(DEFAULT_ANY_PATH);
+	}
+
+	@Test
+	public void prune() throws Exception {
+		assertThat(new JsonPathFilter(-1, null, new String[]{DEFAULT_PATH}), ARRAY_FILTER).hasPruned(DEFAULT_PATH);
+		assertThat(new JsonPathFilter(-1, null, new String[]{DEFAULT_PATH, PASSTHROUGH_XPATH}), ARRAY_FILTER).hasPruned(DEFAULT_PATH);
+		assertThat(new JsonPathFilter(-1, null, new String[]{DEEP_PATH3, PASSTHROUGH_XPATH}), ARRAY_FILTER).hasPruned(DEEP_PATH3);
+	}
+	
+	/*
+	@Test
+	public void pruneMaxPathMatches() throws Exception {
+		assertThat(new MultiPathMaxStringLengthJsonFilter(-1, 1, null, new String[]{"/key3"})).hasPruned("/key3");
+		
+		assertThat(new MultiPathMaxStringLengthJsonFilter(-1, 1, null, new String[]{DEFAULT_PATH})).hasPruned(DEFAULT_PATH);
+		assertThat(new MultiPathMaxStringLengthJsonFilter(-1, 2, null, new String[]{DEFAULT_PATH})).hasPruned(DEFAULT_PATH);
+	}
+	*/
+
+	@Test
+	public void pruneWildcard() throws Exception {
+		assertThat(new JsonPathFilter(-1, null, new String[]{DEFAULT_WILDCARD_PATH}), ARRAY_FILTER).hasPruned(DEFAULT_WILDCARD_PATH);
+	}
+	
+	@Test
+	public void pruneAny() throws Exception {
+		assertThat(new JsonPathFilter(-1, null, new String[]{DEFAULT_ANY_PATH}), ARRAY_FILTER).hasPruned(DEFAULT_ANY_PATH);
+	}	
+
+	@Test
+	public void maxStringLength() throws Exception {
+		assertThat(new JsonPathFilter(DEFAULT_MAX_STRING_LENGTH, null, null), UNICODE_FILTER).hasMaxStringLength(DEFAULT_MAX_STRING_LENGTH);
+	}
+	/*
+	@Test
+	public void maxStringLengthAnonymizePrune() throws Exception {
+		assertThat(new JsonPathFilter(DEFAULT_MAX_STRING_LENGTH, 2, new String[]{"/key1"}, new String[]{"/key3"}))
+			.hasMaxStringLength(DEFAULT_MAX_STRING_LENGTH)
+			.hasMaxPathMatches(2)
+			.hasPruned("/key3")
+			.hasAnonymized("/key1");
+	}
+*/
+}
